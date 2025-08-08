3f26465320e96e5b97941a7a14a7490943018c26
gayathriT <98060548+gayathriTGit@users.noreply.github.com>
2025-08-07 23:11:29 -0700

    Update DatabaseConfig.java

--- src/main/java/com/example/springbootmvcjdbc/config/DatabaseConfig.java
+++ src/main/java/com/example/springbootmvcjdbc/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.example.springbootmvcjdbc.config;
 
+import com.example.springbootmvcjdbc.SpringBootMvcJdbcApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,16 +9,29 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @EnableTransactionManagement
 public class DatabaseConfig {
 
     // Configure your database path here
-    private static final String DB_PATH = "/Users/ritet/OneDrive/Documents/Coding/Caltech/FSD bootcamp/course 3/spring-boot-mvc-jdbc/src/main/resources/db/students_db.db";
+    private static String DB_PATH = "" ; //"/Users/ritet/OneDrive/Documents/Coding/Caltech/FSD bootcamp/course 3/spring-boot-mvc-jdbc/src/main/resources/db/students_db.db";
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws URISyntaxException {
+
+        try {
+            URL resourceUrl = SpringBootMvcJdbcApplication.class.getClassLoader().getResource("db/students_db.db");
+            Path resourcePath = Paths.get(resourceUrl.toURI());
+            System.out.println("Resource Path: " + resourcePath.toAbsolutePath());
+            DB_PATH = resourcePath.toAbsolutePath().toString();
+        }catch(URISyntaxException ex){
+            System.out.println(ex.getMessage());
+        }
         org.sqlite.SQLiteDataSource dataSource = new org.sqlite.SQLiteDataSource();
         dataSource.setUrl("jdbc:sqlite:" + DB_PATH);
 
